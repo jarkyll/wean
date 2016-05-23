6a5056ae1287b2c385a25c8c3b6c49183fa2beb3
Nabeel Virani <nabeelvirani@gmail.com>
2016-05-23 04:36:34 -0500

    only angular, added upvoting and downvoting

---
+++ app.js
@@ -0,0 +1,31 @@
+var app = angular.module("news" ,[]);
+
+app.controller("MainCtrl", ["$scope", function($scope){
+	$scope.greeting = "Hello World!"
+
+	$scope.posts = [
+		{title: 'post 1', upvotes: 5},
+  		{title: 'post 2', upvotes: 2},
+  		{title: 'post 3', upvotes: 15},
+  		{title: 'post 4', upvotes: 9},
+  		{title: 'post 5', upvotes: 4}
+	];
+
+	$scope.addPost = function(){
+		if(!$scope.title || $scope.title === ""){
+			return;
+		}
+		$scope.posts.push({title: $scope.title, upvotes: 0})
+		// make the input box aka $scope.title blank after submit
+		$scope.title = ""
+	};
+
+	$scope.upvote = function(post){
+		post.upvotes = post.upvotes + 1
+	}
+
+	$scope.downvote = function(post){
+		post.upvotes = post.upvotes - 1
+	}
+
+}]);
